7f7448c1400ebce610847a5125e069b955f7363e
Jack Ploetz <jwploetz@gmail.com>
2022-08-06 00:30:13 -0400

    finally done with it all

--- Assignment_5/Assignment_5.java
+++ Assignment_5/Assignment_5.java
@@ -14,6 +14,22 @@ public class Assignment_5 {
 
         System.out.println("The first node is: " + list.front());
         System.out.println("The size of the list is: " + list.size());
+        
+        System.out.println("The last node of the list is: " + list.get_back() + "\n");
+        list.insert_back(4);
+        list.display();
+        list.delete_back();
+        list.display();
+
+        if(list.search(2))
+            System.out.println("Target is found!\n");
+        else
+            System.out.println("Target NOT found!\n");
+    
+        list.insert(2, 9);
+        list.display();
+        list.remove(9);
+        list.display();
     }
 
 }
@@ -76,38 +92,70 @@ class MyList{
     public void display()
     {
         Node temp;
-        for(temp=head; temp!=null ; temp=temp.next)
+        for(temp=head; temp!=null; temp=temp.next)
             System.out.println("->" + temp.data);
         System.out.println();
     }
+
+    // ********************** NEW CODE **********************
+
     // f        *method n that returns last item in list*
     public int get_back()
     {
-
+        Node last = head;
+        while(last.next!=null)
+            last = head.next;
+        return last.data;
     }
     // g        *method that inserts insert_me as the last item in the list*
     public void insert_back(int insert_me)
     {
-
+        Node newNode = new Node(insert_me);
+        Node temp = head;
+        newNode.next = null;
+        while(temp.next!=null)
+            temp=temp.next;
+        temp.next = newNode;
+        size++;
     }
     // h        *method that deletes the last item in the list*
     public void delete_back()
     {
-
+        Node temp = head;
+        while (temp.next.next != null)
+            temp = temp.next;
+        temp.next = null;
+        size--;
     }
     // i        *method that returns true if target is in the list and false otherwise*
     public boolean search(int target)
     {
-
+        Node temp;
+        boolean i = false;
+        for(temp=head; temp!=null; temp=temp.next)
+            if(temp.data==target)
+                i = true;
+        return i;
     }
     // j        *method that inserts insert_me behind after_me item in the list (assume no duplicates in the linked list)*
     public void insert(int after_me, int insert_me)
     {
-
+        Node temp = head;
+        Node newNode = new Node(insert_me);
+        while(temp.data!=after_me)
+            temp = temp.next;
+        newNode.next = temp.next;
+        temp.next = newNode;
     }
     // k        *method that removes the delete_me item in the list*
     public void remove(int delete_me)
     {
+        Node temp = head;
+        while (temp.data != delete_me)
+            temp = temp.next;
+        head.next = temp.next;
 
     }
+
+    // ********************** END NEW CODE **********************
 }
